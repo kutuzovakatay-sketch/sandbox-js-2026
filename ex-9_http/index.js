3197209f15f9ace8315eeee5c2466d89a0d2b520
Kutuzova Katy <kutuzovakatay@gmail.com>
2026-03-25 22:33:38 +0300

    в процессе

--- ex-9_http/index.js
+++ ex-9_http/index.js
@@ -1,13 +1,22 @@
 import {createServer} from 'node:http';
 
 const server = createServer((req,res) =>{
-    console.log('req.headers', req.headers);
+    console.log('request detected', 'method:', req.method, 'url:', req.url);
+    
+    // console.log('req.headers', req.headers);
     res.writeHead(200, {
         'Content-Type': 'text/html',
         'My-Custom-Header-89': 'my-custom-header-value'
     });
 
-    res.end("<b>Hello from node.js server</b>");
+    res.end(`
+        <b>Hello from node.js server</b>
+        <form action = "/" method = "POST">
+        <span>Enter your name</span>
+        <input type = "text" name = "username">
+        <button>Send</button>
+        </form>
+    `);
 } );
 
 server.listen(3000);
